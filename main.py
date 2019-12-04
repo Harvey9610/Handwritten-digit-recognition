f40dfe4ed82917b846cee2f8291fcd64d54a1f46
黄维 <hw@huangweideMacBook-Pro.local>
2019-12-04 20:23:09 +0800

    finish main.py

--- main.py
+++ main.py
@@ -16,36 +16,38 @@ use_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
 kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
 
-transform=transforms.Compose([
+transform = transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
-            ]))
+                transforms.Normalize((0.1307,), (0.3081,))])
 
-train_datasets = MNIST(root='./data/',
-                      train=True,
-                      transform=transform,
-                      download=True)
+train_datasets = MNIST(
+    root='./data/',
+    train=True,
+    transform=transform,
+    download=True)
 
-test_datasets = MNIST(root='./data/',
-                     train=False,
-                     transform=transform,
-                     download=True)
+test_datasets = MNIST(
+    root='./data/',
+    train=False,
+    transform=transform,
+    download=True)
 
-train_loader = torch.utils.data.DataLoader(train_datasets,
-                                           batch_size=batch_size,
-                                           shuffle=True,
-                                           **kwargs)
+train_loader = torch.utils.data.DataLoader(
+    train_datasets,
+    batch_size=batch_size,
+    shuffle=True,
+    **kwargs)
 
-test_loader = torch.utils.data.DataLoader(test_datasets,
-                                          batch_size=batch_size,
-                                          shuffle=True,
-                                          **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    test_datasets,
+    batch_size=batch_size,
+    shuffle=True,
+    **kwargs)
 
 def train(epoch, model, train_loader, criterion, optimizer):
     model.train()
     total_loss = 0
     correct = 0
-    t0 = time.time()
     for _, (images, labels) in enumerate(train_loader, start=1):
         print('Epochs: [%d/%d]' % (epoch, epochs))
         images = images.to(device)
@@ -63,14 +65,12 @@ def train(epoch, model, train_loader, criterion, optimizer):
         optimizer.step()
 
     accuracy = correct / len(train_datasets)
-    t1 = time.time()
-    print('epoch: [%d/%d]\ntime: %d\ttrain_loss: %.4f\taccuracy: %.4f' % (epoch, epochs, (t1-t0), total_loss.item(), accuracy))
+    return total_loss, accuracy
 
 def test(epoch, model, test_loader, criterion):
     model.eval()
     total_loss = 0
     correct = 0
-    t0 = time.time()
     with torch.no_grad():
         for _, (images, labels) in enumerate(test_loader, start=1):
             images = images.to(device)
@@ -84,8 +84,7 @@ def test(epoch, model, test_loader, criterion):
             total_loss += loss
 
     accuracy = correct / len(test_datasets)
-    t1 = time.time()
-    print('epoch: [%d/%d]\ntime: %d\tloss: %.4f\taccuracy: %.4f' % (epoch, epochs, (t1-t0), total_loss.item(), accuracy))
+    return total_loss, accuracy
 
 def main():
     model = Model()
@@ -97,11 +96,19 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
 
-    for epoch in range(epochs):
-        train(epoch, model, train_loader, criterion, optimizer)
-        test(epoch, model, test_loader, criterion)
+    criterion = criterion.to(device)
+
+    train_loss = 0.
+    train_acc = 0.
+    test_loss = 0.
+    test_acc = 0.
+
+    for epoch in range(1, epochs+1):
+        train_loss, train_acc = train(epoch, model, train_loader, criterion, optimizer)
+        test_loss, test_acc = test(epoch, model, test_loader, criterion)
+        print('epoch: [%d/%d]\ttrain_loss: %.4f\ttrain_acc: %.4f\ttest_loss: %.4f\t test_acc: %.4f' % (epoch, epochs, train_loss, train_acc, test_loss, test_acc))
 
     model.save('mnist.pkt')
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
